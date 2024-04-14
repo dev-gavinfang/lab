c1d4eea9b324c3c4a02eedfd072e0044f5e21608
Gavin Fang <gavinfang@hotmail.com>
2024-04-14 07:30:45 +0300

    Async enumerable (#1)
    * api returns 'IAsynEnumerable<T>' and bazor based demo
    
    * cleanup
    
    * js implementation
    
    * Cancellation support

---
+++ src/AsyncEnumerableSample/AsyncEnumerableSample.Client/Contracts/SampleDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AsyncEnumerableSample.Client.Contracts;
+
+public class SampleDto
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
