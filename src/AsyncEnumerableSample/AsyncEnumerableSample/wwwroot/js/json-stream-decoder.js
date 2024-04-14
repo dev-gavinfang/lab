9b425c7a74ddf8a0c29a9594a7e1892e4589f535
Gavin Fang <gavinfang@hotmail.com>
2024-04-14 07:09:40 +0300

    js implementation

---
+++ src/AsyncEnumerableSample/AsyncEnumerableSample/wwwroot/js/json-stream-decoder.js
@@ -0,0 +1,61 @@
+﻿"use strict";
+class JsonStreamDecoder {
+    constructor() {
+        this.level = 0;
+        this.partialItem = '';
+        this.decoder = new TextDecoder();
+    }
+    
+    decodeChunk(value) {
+        const chunk = this.decoder.decode(value);
+        let itemStart = 0;
+        for (let i = 0; i < chunk.length; i++) {
+            if (chunk[i] === JTOKEN_START_OBJECT) {
+                if (this.level === 0) {
+                    itemStart = i;
+                }
+                this.level++;
+            }
+            if (chunk[i] === JTOKEN_END_OBJECT) {
+                this.level--;
+                if (this.level === 0) {
+                    let item = chunk.substring(itemStart, i + 1);
+                    if (this.partialItem) {
+                        item = this.partialItem + item;
+                        this.partialItem = '';
+                    }
+
+                    return JSON.parse(item);
+                }
+            }
+        }
+        if (this.level !== 0) {
+            this.partialItem = chunk.substring(itemStart);
+        }
+    }
+}
+
+const JTOKEN_START_OBJECT = '{';
+const JTOKEN_END_OBJECT = '}';
+
+window.fetchAsyncEnumerable = async (dotnetHelper) => {
+    const url = "/api/async-enumerable";
+    const response = await fetch(url);
+    const reader = response.body.getReader();
+
+    if (!reader) {
+        throw new Error('Failed to read response');
+    }
+
+    const decoder = new JsonStreamDecoder();
+
+    while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        if (!value) continue;
+        const chunk = decoder.decodeChunk(value);
+        dotnetHelper.invokeMethodAsync('OnDataReceived', chunk);
+    }
+
+    reader.releaseLock();
+}
